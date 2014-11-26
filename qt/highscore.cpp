8f537a6f8d27c7a9f47dcc90ccb71747efebd1ca
kerso255 <me.kerstin@live.se>
2014-11-26 15:24:24 +0100

    highscore

--- qt/highscore.cpp
+++ qt/highscore.cpp
@@ -1,5 +1,39 @@
+//highscore.cpp
 #include "highscore.h"
 
 highscore::highscore()
 {
 }
+
+//Anropas när det har blivit game over.
+//Sparar (ev.) poängen till highscore-listan och sedan till filen.
+void highscore::save()
+{
+}
+
+//Anropas när "highscore" väljs i startmenyn.
+//Laddar in highscore-listan och visar den på skärmen.
+void highscore::load()
+{
+
+}
+
+//Borde vi inte t.ex. ha en sådan här funktion i private i highscore.h?
+void highscore::insert(int score)
+{
+    //TODO: Loop som går igenom och jämför poängen i listan med de nya poängen,
+    //och lägger in scoren på den plats som de ska ligga.
+    /*
+    if (!find(name))
+    {
+        v_table.insert(pair<string, double> (name, value));
+    }
+    */
+}
+
+//Tar bort sista så att listan fortsätter att vara 10 lång?
+void highscore::remove_last()
+{
+    //v_table.erase(name);
+}
+
--- qt/highscore.h
+++ qt/highscore.h
@@ -1,10 +1,23 @@
 #ifndef HIGHSCORE_H
 #define HIGHSCORE_H
 
+#include <map>
+#include <string>
+
 class highscore
 {
 public:
-    highscore();
+    highscore() = default;
+    ~highscore() = default;
+
+    highscore(const highscore&) = default;
+
+    void load();
+    void save();
+
+private:
+    std::map<int, int> list_of_score;
+
 };
 
 #endif // HIGHSCORE_H
